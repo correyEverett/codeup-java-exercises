db3791b4722771bb58f78491c0dd8e0548b71d5c
Correy Everett <correy.everett@gmail.com>
2020-12-02 10:52:27 -0600

    unary operator placement difference of outcome

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,14 +15,27 @@ public class HelloWorld {
 
 //        myString = 3.14159; compiler error: incompatible types: double cannot be converted to java.lang.String
 
-        long myNumber;
+//        long myNumber;
 //        System.out.println(myNumber); compiler error: variable myNumber might not have been initialized
 
 //        myNumber = 3.14; compiler error: incompatible types: possible lossy conversion from double to long
 
-        myNumber = 123L;
+//        myNumber = 123L;
 
-        myNumber = 123;
+//        myNumber = 123;
+
+//        float myNumber = 3.14; compiler error: incompatible types: possible lossy conversion from double to float
+//            POSSIBLE FIXES BELOW:
+//            double myNumber = 3.14;
+//            float myNumber = Float.parseFloat("3.14");
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
     }
 
