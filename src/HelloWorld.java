111d9d3ed2150ec88a4f17f5ff8eb243e25d0353
Correy Everett <correy.everett@gmail.com>
2020-12-02 11:09:02 -0600

    rewrite expressions using shorthand assignment operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -42,6 +42,28 @@ public class HelloWorld {
 //        ';' expected
 //        <identifier> expected
 //        reached end of file while parsing
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
     }
 
 }
