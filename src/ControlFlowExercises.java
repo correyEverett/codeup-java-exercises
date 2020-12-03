062ba3ac7e57ff00b73d1fdd7946416a8dd94cf2
Correy Everett <correy.everett@gmail.com>
2020-12-03 15:04:41 -0600

    create a do while loop that counts by 2s starting at 0 and ending at 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,51 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("The current value of i is:" + i);
-            i++;
-        }
+//        DO WHILE LOOP
+        int i = 0;
+//        boolean isEven = i % 2 == 0;
+
+        do {
+                System.out.printf("I is equal to %d%n", i);
+                i += 2;
+        }while (i <= 100);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WHILE LOOP
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("The current value of i is:" + i);
+//            i++;
+//        }
