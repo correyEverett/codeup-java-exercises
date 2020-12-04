5770bd606657fab48c91e8433a19807296904dee
Correy Everett <correy.everett@gmail.com>
2020-12-04 15:27:12 -0600

    create four seperate methods. each will perform an arithmatic operation

---
+++ src/MethodsExercises.java
@@ -0,0 +1,28 @@
+
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+        System.out.println(Modulus(10, 5));
+    }
+
+    public static int Addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int Subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int Multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int Division(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static int Modulus(int num1, int num2) {
+        return num1 % num2;
+    }
+
+}
