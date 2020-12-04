ff45abecc50e4f28cc3b1d682501e647070fb92c
Correy Everett <correy.everett@gmail.com>
2020-12-04 11:16:22 -0600

    create string variable and print "windows"

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -4,8 +4,12 @@ public class StringExercise {
         String noEd = "We don't need no education \nWe don't need no thought control";
         System.out.println(noEd);
 
-        String check = "Check this out!, s inside of s!";
+        String check = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(check);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
 
 
     }
