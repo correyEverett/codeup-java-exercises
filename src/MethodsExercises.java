7042e50987e378ca78cb8d4896556eaeb90fa037
Correy Everett <correy.everett@gmail.com>
2020-12-04 17:30:57 -0600

    use recursion for the multiplication method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,9 +2,14 @@
 public class MethodsExercises {
 
     public static void main(String[] args) {
+        System.out.println(Addition(10, 5));
+        System.out.println(Subtraction(10, 5));
+        System.out.println(Multiplication(10, 5));
+        System.out.println(Division(10, 5));
         System.out.println(Modulus(10, 5));
     }
 
+    //ARITHMETIC METHODS
     public static int Addition(int num1, int num2) {
         return num1 + num2;
     }
@@ -14,7 +19,10 @@ public class MethodsExercises {
     }
 
     public static int Multiplication(int num1, int num2) {
-        return num1 * num2;
+        if (num2 <= 0) {
+            return 0;
+        }
+        return num1 + Multiplication(num1, num2 - 1);
     }
 
     public static int Division(int num1, int num2) {
@@ -23,6 +31,11 @@ public class MethodsExercises {
 
     public static int Modulus(int num1, int num2) {
         return num1 % num2;
+    }
+    //INPUT RANGE VALIDATION
+    public static int getInteger(int min, int max) {
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
     }
 
 }
