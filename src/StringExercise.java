30cce8ebd4b893ebc9348876388ec717a97206a0
Correy Everett <correy.everett@gmail.com>
2020-12-04 10:53:46 -0600

    start-day

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String noEd = "We don't need no education \nWe don't need no thought control";
+        System.out.println(noEd);
+
+
+    }
+}
