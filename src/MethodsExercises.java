6028e0e6bfb7414424e5aaae343e6ebefcf0e5a2
Correy Everett <correy.everett@gmail.com>
2020-12-07 11:54:44 -0600

    CALCULATE THE FACTORIAL OF A NUMBER

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -9,7 +9,8 @@ public class MethodsExercises {
 //        System.out.println(Multiplication(10, 5));
 //        System.out.println(Division(10, 5));
 //        System.out.println(Modulus(10, 5));
-        getInteger(1, 10);
+//        getInteger(1, 10);
+        System.out.println("This is the result of Calculate Factorial: " + calculateFactorial());
     }
 
     //ARITHMETIC METHODS
@@ -35,19 +36,49 @@ public class MethodsExercises {
 //    public static int Modulus(int num1, int num2) {
 //        return num1 % num2;
 //    }
-    //INPUT RANGE VALIDATION
+    //INPUT RANGE VALIDATION USING ITERATION
+//    public static int getInteger(int min, int max) {
+//        Scanner scan = new Scanner(System.in);
+//        int userNumber;
+//
+//        do{
+//        System.out.println("Enter a number between 1 and 10: ");
+//        userNumber = scan.nextInt();
+//        }while(userNumber < min || userNumber > max);
+//
+//        System.out.println(userNumber);
+//        return userNumber;
+//    }
+
+    //INPUT RANGE VALIDATION USING RECURSION
     public static int getInteger(int min, int max) {
-        System.out.print("Enter a number between 1 and 10:\n ");
         Scanner scan = new Scanner(System.in);
-        int userNumber = scan.nextInt();
-        do {
-            getInteger(1, 10);
-            continue;
-        } while(userNumber < min | userNumber > max);
+        System.out.printf("Enter a number between %d and %d:\n", min, max);
+        int userNumber;
+        userNumber = scan.nextInt();
+
+        if(userNumber < min || userNumber > max) {
+            return getInteger(min, max);
+        }
         System.out.println(userNumber);
         return userNumber;
+
     }
 
+
+    //CALCULATE THE FACTORIAL OF A NUMBER
+    public static int calculateFactorial() {
+
+        System.out.println("Enter a number between 1 and 10:");
+        int userNumber = getInteger(1, 10);
+        int fact = 1;
+
+        for (int i = 1; i <= userNumber; i++) {
+            fact = fact * i;
+            System.out.printf("%d! = %d\n", i, fact);
+        }
+        return fact;
+    }
 
 
 
