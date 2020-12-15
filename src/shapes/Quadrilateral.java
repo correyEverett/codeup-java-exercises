f2255c3fca07f2f01829fb8e5af2951cd64b86ef
Correy Everett <correy.everett@gmail.com>
2020-12-15 14:19:09 -0600

    create PROTECTED properties and Quadrilateral CONSTRUCTOR

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,13 @@
+package shapes;
+
+abstract class Quadrilateral extends Shape implements Measurable {
+
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+}
