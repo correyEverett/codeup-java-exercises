c82378872d4ceba10dff88bbaa4fa339cafab53d
Correy Everett <correy.everett@gmail.com>
2020-12-15 14:25:44 -0600

    create methods for SETTING the length and the width

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,20 +1,28 @@
 package shapes;
 
 abstract class Quadrilateral extends Shape implements Measurable {
-
+    //Properties
     protected double length;
     protected double width;
-
+    //Constructor
     public Quadrilateral(double length, double width) {
         this.length = length;
         this.width = width;
     }
-
+    //Getters
     public double getLength() {
         return length;
     }
 
     public double getWidth() {
         return width;
+    }
+    //Setters
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
     }
 }
