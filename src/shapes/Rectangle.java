fc78088c3c26b8bb7f780f80c57436175c71fe6e
Correy Everett <correy.everett@gmail.com>
2020-12-15 13:55:20 -0600

    create an abstract shape class

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,19 +2,25 @@ package shapes;
 
 public class Rectangle {
 
-    protected int length;
-    protected int width;
+//    protected int length;
+//    protected int width;
+//
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public int getPerimeter() {
+//        return (length + width) * 2;
+//    }
+//
+//    public int getArea (){
+//        return length * width;
+//    }
 
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
 
-    public int getPerimeter() {
-        return (length + width) * 2;
-    }
 
-    public int getArea (){
-        return length * width;
-    }
+
+
+
 }
