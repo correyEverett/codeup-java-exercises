a2e7b38a8477063a0705168dc6998952fecbd5dd
Correy Everett <correy.everett@gmail.com>
2020-12-16 20:04:53 -0600

    create methods in Student class

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -11,9 +11,25 @@ public class Student {
         this.name = name;
     }
 
+    public void setGrades(ArrayList<Integer> grades) {
+        ArrayList<Integer> grades = new ArrayList<>();
+    }
 
-
-
+    // returns the student's name
+    public String getName(String name) {
+        return this.name = name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(ArrayList<Integer> grades) {
+        for (int grade : grades) {
+            return grade += grade;
+        }
+        return grade / this.grades.size();
+    }
 
 
 
